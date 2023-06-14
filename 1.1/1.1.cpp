2d7c488fac719207f94dadf3ba502ef424876856
Александр Канов <monersen@gmail.com>
2023-06-14 18:44:05 +0300

    Добавьте файлы проекта.

---
+++ 1.1/1.1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(int argc, char const* argv[])
+{
+    vector<int> v{4, 7, 9, 14, 12};
+
+    cout << "Input values:\t";
+    for_each(v.begin(), v.end(), [](const int& a) {
+        cout << a << " ";
+        });
+
+    cout << "\nOutput values:\t";
+    for_each(v.begin(), v.end(), [](const int& a) {
+        cout << (a % 2 ? a * 3 : a) << " ";
+        });
+
+    cout << endl;
+    return 0;
+}
---
+++ 1.3/1.3.cpp
@@ -0,0 +1,53 @@
+﻿#include <cmath>
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <functional>
+
+const double Pi = 3.1415926;
+
+void printAngles(const std::vector<int>& angles) {
+	for (const auto& angle : angles) {
+		if (angle != angles.back()) {
+			std::cout << angle << " deg (" << (angle * Pi / 180) << " rad), ";
+		}
+		else {
+			std::cout << angle << " deg (" << (angle * Pi / 180) << " rad). ";
+		}
+	}
+}
+
+void printOutput(const std::vector<int>& angles, std::vector<std::function<void(int)>> functions) {
+	for (const auto& angle : angles) {
+		std::cout << angle << " deg : ";
+		for (const auto& function : functions) {
+			function(angle);
+		}
+		std::cout << std::endl;
+	}
+};
+
+
+int main(int argc, char** argv) {
+
+	std::vector<int> angles = { 30, 60, 90 };
+
+	std::cout << "GIVEN ANGLES: ";
+	printAngles(angles);
+
+	std::cout << std::endl << std::endl;
+
+	std::function<void(int)> cosine = [](const int& angle) -> void {
+		std::cout << std::setprecision(6) << "cos = " << std::cos(angle * Pi / 180) << '\t';
+	};
+	std::function<void(int)> sine = [](const int& angle) -> void {
+		std::cout << std::setprecision(6) << "sin = " << std::sin(angle * Pi / 180) << '\t';
+	};
+
+	auto functions = { sine, cosine };
+
+	std::cout << "OUTPUT:\n";
+	printOutput(angles, functions);
+
+	return EXIT_SUCCESS;
+}
