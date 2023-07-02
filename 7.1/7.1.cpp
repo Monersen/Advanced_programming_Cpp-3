56d35959fa3ec66eb437939f473194f893496170
Александр Канов <monersen@gmail.com>
2023-07-02 18:32:04 +0300

    STL. Часть 2

---
+++ 7.1/7.1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+template<typename T>
+void uniqme(vector<T>& v) {
+    set<T> sTemp(v.begin(), v.end());
+    v.assign(sTemp.begin(), sTemp.end());
+}
+
+template<typename T>
+void print(const vector<T>& v) noexcept {
+    for (const auto& i : v)
+        cout << i << " ";
+    cout << endl;
+}
+
+int main(int argc, char const* argv[])
+{
+    vector<int> v = { 1, 1, 2, 5, 6, 1, 2, 4 };
+
+    cout << "[IN]: ";
+    print(v);
+
+    uniqme(v);
+    cout << "[OUT]: ";
+    print(v);
+
+    return 0;
+}
---
+++ 7.2/7.2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+#include <string>
+#include <list>
+using namespace std;
+
+template<typename T>
+void print_container(const T& cnt) {
+
+    for (auto it = cnt.begin(); it != cnt.end(); ++it)
+        cout << *it << " ";
+    cout << endl;
+}
+
+int main(int argc, char const* argv[])
+{
+    set<string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set);
+
+    list<string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list);
+
+    vector<string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector);
+
+    return 0;
+}
