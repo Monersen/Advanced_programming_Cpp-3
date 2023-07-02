01bef1026f841a1fec6424c0c9c126eab0b363e6
Александр Канов <monersen@gmail.com>
2023-07-02 18:31:55 +0300

    STL. Часть 1

---
+++ 6.1/6.1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <string>
+using namespace std;
+
+template<typename A, typename B>
+pair<B, A> flip_pair(const pair<A, B>& p)
+{
+	return pair<B, A>(p.second, p.first);
+}
+
+template<typename A, typename B>
+multimap<B, A> flip_map(const map<A, B>& src)
+{
+	multimap<B, A> dst;
+	transform(src.rbegin(), src.rend(), inserter(dst, dst.begin()),
+		flip_pair<A, B>);
+	return dst;
+}
+
+int main()
+{
+	const char* str = "Hello world!!";
+	map<char, int> frequency;
+
+	for (int i = 0; i < strlen(str); i++)
+	{
+		const char value = str[i];
+		int count = 0;
+		for (int j = 0; j < strlen(str); j++)
+		{
+			if (value == str[j])
+			{
+				count += 1;
+			}
+		}
+		frequency[value] = count;
+	}
+	multimap<int, char> dst = flip_map(frequency);
+	for (multimap<int, char>::reverse_iterator itr = dst.rbegin(); itr != dst.rend(); itr++)
+	{
+		cout << itr->first << ": " << itr->second << endl;
+	}
+}
---
+++ 6.2/6.2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+using namespace std;
+
+int main()
+{
+    int n = 0, nn;
+    set<int> v;
+
+    cout << "[IN]: " << endl;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> nn;
+        v.insert(nn);
+    }
+
+    vector<int> as(v.begin(), v.end());
+    sort(as.begin(), as.end(), [](const int& a, const int& b) {
+        return a > b;
+        });
+
+    cout << "[OUT]: " << endl;
+    for (const auto& i : as)
+        cout << i << endl;
+
+    return 0;
+}
