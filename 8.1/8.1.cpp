1d4f80751f8e6005b845968db8a542dab9aff99c
Александр Канов <monersen@gmail.com>
2023-07-17 15:42:39 +0300

    Умные указатели

---
+++ 8.1/8.1.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory>
+using namespace std;
+
+class tridiagonal_matrix
+{
+public:
+    vector<double> m_down;
+    vector<double> m_upper;
+    vector<double> m_middle;
+    tridiagonal_matrix(
+        const vector<double>& down,
+        const vector<double>& upper,
+        const vector<double>& middle) :
+        m_down{ down }, m_upper{ upper }, m_middle{ middle }
+    {};
+    ~tridiagonal_matrix() { cout << "destructor called\n"; }
+
+    unique_ptr<tridiagonal_matrix> clone() {
+
+        auto ptr = make_unique<tridiagonal_matrix>(m_down, m_upper, m_middle);
+        if (!ptr)
+            throw bad_alloc();
+
+        return ptr;
+    }
+};
+
+int main()
+{
+    auto down = vector<double>{ -4.0, 5.0 };
+    auto upper = vector<double>{ 14.0, 8.0 };
+    auto middle = vector<double>{ 3.0, 1.0, 7.0 };
+    auto matrix = make_unique<tridiagonal_matrix>(down, upper, middle);
+
+    if (!matrix) {
+        cerr << "Error creating unique ptr!\n";
+        return -1;
+    }
+
+    try {
+        auto matrix_clone = matrix->clone();
+    }
+    catch (exception& ex) {
+        cerr << ex.what() << endl;
+    }
+
+
+    return 0;
+}
---
+++ 8.2/8.2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory>
+#include <unordered_map>
+using namespace std;
+
+using namespace std;
+
+class node
+{
+public:
+    int m_value;
+    weak_ptr<node> parent;
+    node(int value) : m_value{ value } {};
+    ~node() { cout << "destructor called\n"; }
+};
+
+int main()
+{
+    {
+        auto node1 = make_shared<node>(1);
+        if (!node1) {
+            cerr << "Unable to create shared ptr\n";
+            return -1;
+        }
+
+        auto node2 = make_shared<node>(2);
+        if (!node2) {
+            cerr << "Unable to create shared ptr\n";
+            return -1;
+        }
+    }
+
+    return 0;
+}
